7cea353389fbeee7406263f3aac12d3600ef5381
Anshuman <chaoanxuman999@gmail.com>
2025-07-12 23:06:44 +0530

    Express JS models

---
+++ User/Tasks.js
@@ -0,0 +1,11 @@
+class Tasks{
+    constructor(title,description,assignedUser,status,priority){
+        this.title=title;
+        this.description=description;
+        this.assignedUser=assignedUser;
+        this.status=status;
+        this.priority=priority;
+    }
+}
+
+module.exports=Tasks;
---
+++ User/user.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(id,name,email,tasks){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.tasks=tasks;
+    }
+}
+
+module.exports=User;
